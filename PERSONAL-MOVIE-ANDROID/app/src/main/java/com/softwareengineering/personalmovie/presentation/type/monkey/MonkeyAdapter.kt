9af43c366e3e2d612a9613a9e8cd08d9be5f9d06
Tyee22 <oth9090@naver.com>
2024-05-13 22:27:38 +0900

    유형별 화면 구현

---
+++ PERSONAL-MOVIE-ANDROID/app/src/main/java/com/softwareengineering/personalmovie/presentation/type/monkey/MonkeyAdapter.kt
@@ -0,0 +1,43 @@
+package com.softwareengineering.personalmovie.presentation.type.monkey
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.softwareengineering.personalmovie.R
+import com.softwareengineering.personalmovie.data.Movie
+import com.softwareengineering.personalmovie.databinding.ItemTypeBinding
+
+class MonkeyAdapter(private val context:Context, private val movieList:List<Movie>):
+    RecyclerView.Adapter<MonkeyAdapter.MonkeyPagerViewHolder>() {
+    inner class MonkeyPagerViewHolder(private val binding: ItemTypeBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(movie:Movie) {
+            with(binding){
+                tvYear.text=movie.year.toString()
+                ivVideo.load(movie.video)
+                tvName.text=movie.name
+                tvGenre.text=movie.tag.toString()
+                tvScore.text=context.getString(R.string.score, movie.grade)
+                // tv_score의 텍스트 값을 가져와서 Float 값으로 변환하여 레이팅으로 설정
+                val scoreText = movie.grade.toFloat()
+                rbRating.rating = scoreText
+            }
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MonkeyPagerViewHolder {
+        val binding =
+            ItemTypeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MonkeyPagerViewHolder(binding)
+    }
+    override fun onBindViewHolder(holder: MonkeyPagerViewHolder, position: Int) {
+        val movie = movieList[position]
+        holder.bind(movie)
+    }
+
+    override fun getItemCount(): Int =movieList.size
+}
